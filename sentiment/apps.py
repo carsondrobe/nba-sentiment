155187eef4a7e276db42ffa7ec92b4fa2063db80
carsondrobe <carsondrobe@gmail.com>
2023-12-16 13:21:00 -0800

    sentiment analysis for sentences

--- sentiment/apps.py
+++ sentiment/apps.py
@@ -2,5 +2,5 @@ from django.apps import AppConfig
 
 
 class SentimentConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'sentiment'
+    default_auto_field = "django.db.models.BigAutoField"
+    name = "sentiment"
--- sentiment/views.py
+++ sentiment/views.py
@@ -1,3 +1,19 @@
 from django.shortcuts import render
+from nltk.sentiment import SentimentIntensityAnalyzer
 
-# Create your views here.
+
+def analyze_sentiment(request):
+    if request.method == "POST":
+        text = request.POST.get("text", "")
+        sia = SentimentIntensityAnalyzer()
+        sentiment_scores = sia.polarity_scores(text)
+
+        return render(
+            request,
+            "sentiment/sentiment_analysis.html",  # Adjust the path based on your template structure
+            {"text": text, "sentiment_scores": sentiment_scores},
+        )
+
+    return render(
+        request, "sentiment/sentiment_analysis.html"
+    )  # Adjust the path based on your template structure
